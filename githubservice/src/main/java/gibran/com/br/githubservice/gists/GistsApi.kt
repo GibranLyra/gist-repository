e4b7adcbe9aa8d90e85980ef04eb821aab30db70
Gibran Lyra <gibran.lyra@quality.com.br>
2018-01-09 14:21:39 -0200

    added services and apis for gistApi

---
+++ githubservice/src/main/java/gibran/com/br/githubservice/gists/GistsApi.kt
@@ -0,0 +1,30 @@
+package gibran.com.br.githubservice.gists
+
+import gibran.com.br.githubservice.GitHubApiModule
+import gibran.com.br.githubservice.model.Gist
+import io.reactivex.Observable
+import timber.log.Timber
+
+/**
+ * Created by gibranlyra on 09/01/18 for gist_consumer.
+ */
+
+object GistsApi {
+    private val gistsService: GistsService
+
+    init {
+        val retrofit = GitHubApiModule.retrofit
+        gistsService = retrofit.create(GistsService::class.java)
+    }
+
+    fun publicGists(page: Int, perPage: Int = 20)
+            : Observable<List<Gist>> {
+        return gistsService.publicGists(page, perPage)
+                .doOnError { e -> Timber.e(e, "publicGists: %s", e.message) }
+    }
+
+    fun gist(id: String): Observable<Gist> {
+        return gistsService.gist(id)
+                .doOnError { e -> Timber.e(e, "gist: %s", e.message) }
+    }
+}
---
+++ githubservice/src/main/java/gibran/com/br/githubservice/gists/GistsService.kt
@@ -0,0 +1,21 @@
+package gibran.com.br.githubservice.gists
+
+import gibran.com.br.githubservice.model.Gist
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+const val GISTS = "gists/"
+
+/**
+ * Created by gibranlyra on 09/01/18 for gist_consumer.
+ */
+internal interface GistsService {
+    @GET(GISTS.plus("public"))
+    fun publicGists(@Query("page") page: Int,
+                    @Query("perPage") perPage: Int): Observable<List<Gist>>
+
+    @GET(GISTS.plus("{id}"))
+    fun gist(@Path("id") id: String): Observable<Gist>
+}
