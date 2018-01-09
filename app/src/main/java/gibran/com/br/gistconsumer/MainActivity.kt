dc15e17e63cfd9a18aca4e74f34ba1dae34d34e5
Gibran Lyra <gibran.lyra@quality.com.br>
2018-01-09 13:43:17 -0200

    initial commit

---
+++ app/src/main/java/gibran/com/br/gistconsumer/MainActivity.kt
@@ -0,0 +1,12 @@
+package gibran.com.br.gistconsumer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
