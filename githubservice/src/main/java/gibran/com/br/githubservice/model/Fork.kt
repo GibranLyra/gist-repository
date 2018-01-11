7d4d1c5cbf76c22558a582fa67efedb8a1d55655
Gibran Lyra <gibran.lyra@quality.com.br>
2018-01-11 16:51:12 -0200

    removed some unused models

--- githubservice/src/main/java/gibran/com/br/githubservice/model/Fork.kt
+++
@@ -1,7 +0,0 @@
-package gibran.com.br.githubservice.model
-
-data class Fork(var updatedAt: String? = null,
-                var createdAt: String? = null,
-                var id: String? = null,
-                var user: Any? = null,
-                var url: String? = null)
--- githubservice/src/main/java/gibran/com/br/githubservice/model/History.kt
+++
@@ -1,7 +0,0 @@
-package gibran.com.br.githubservice.model
-
-data class History(var committedAt: String? = null,
-                   var changeStatus: HistoryChangeStatus? = null,
-                   var version: String? = null,
-                   var user: Any? = null,
-                   var url: String? = null)
